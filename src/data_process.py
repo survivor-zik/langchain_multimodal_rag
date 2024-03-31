6e8255e00ec9a4ca4e27a5bb87840207de7c3352
survivor <139892449+survivor-zik@users.noreply.github.com>
2024-03-31 06:58:51 +0500

    working

---
+++ src/data_process.py
@@ -0,0 +1,35 @@
+from unstructured.partition.pdf import partition_pdf
+from langchain.text_splitter import SpacyTextSplitter
+
+
+def extract_pdf_elements(path, fname):
+    """
+    Extract images, tables, and chunk text from a PDF file.
+    path: File path, which is used to dump images (.jpg)
+    fname: File name
+    """
+    return partition_pdf(
+        filename=path + fname,
+        extract_images_in_pdf=False,
+        infer_table_structure=True,
+        chunking_strategy="by_title",
+        max_characters=4000,
+        new_after_n_chars=3800,
+        combine_text_under_n_chars=2000,
+        image_output_dir_path=path,
+    )
+
+
+def categorize_elements(raw_pdf_elements):
+    """
+    Categorize extracted elements from a PDF into tables and texts.
+    raw_pdf_elements: List of unstructured.documents.elements
+    """
+    tables = []
+    texts = []
+    for element in raw_pdf_elements:
+        if "unstructured.documents.elements.Table" in str(type(element)):
+            tables.append(str(element))
+        elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
+            texts.append(str(element))
+    return texts, tables
